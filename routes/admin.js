24d1c1385a258f6acaa3067615d417cdc3197257
haattis <exburn0@gmail.com>
2019-02-12 11:17:39 +0100

    Admin match deletion route

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const ObjectId = require('mongoose').Types.ObjectId;
 const User = mongoose.model('User');
 const PreUser = mongoose.model('PreUser');
+const Match = mongoose.model('Match')
+
 
 router.post('/user/activate', function(req, res) {
   const { _id } = req.body
@@ -60,6 +62,65 @@ router.get('/tempusers', function(req, res) {
   })
   .catch(err => {
     return res.status(400).send('Something went wrong')
+  })
+})
+
+router.post('/match/delete', function(req, res){
+  const { _id } = req.body
+
+  Match.findOne({_id: _id})
+  .then(async match => {
+    if(!match) {
+      return res.status(400).send('No match found')
+    }
+    let user1 = await User.findOne({_id: match.player1})
+    let user2 = await User.findOne({_id: match.player2})
+
+    const rankchange1 = match.newrank1 - match.oldrank1
+    const rankchange2 = match.newrank2 - match.oldrank2
+
+    user1.rating = user1.rating - rankchange1
+    user2.rating = user2.rating - rankchange2
+
+    user1.save()
+    .catch(err => {
+      return res.status(400).send('User 1 was not saved, nothing is changed')
+    })
+
+    user2.save()
+    .catch(err => {
+      user1.rating = user1.rating + rankchange1
+      user1.save()
+      .catch(err => {
+        return res.status(400).send('Error. User1 was changed, but not user2')
+      })
+      return res.status(400).send('User 2 was not saved, user 1 reverted')
+    })
+
+    match.remove()
+    .then(function(){
+      return res.status(200).send('Match deleted')
+    })
+    .catch(function(){
+      user1.rating = user1.rating + rankchange1
+      user1.save()
+      .catch(err => {
+        return res.status(400).send('Users was changed, but match was not deleted')
+      })
+      user2.rating = user2.rating + rankchange2
+      user2.save()
+      .catch(err => {
+        return res.status(400).send('User2 was changed, but match was not deleted')
+      })
+      return res.status(400).send('Nothing changed')
+
+    })
+
+
+
+  })
+  .catch(err => {
+    return res.status(400).send('Something went wrong. Did you send a 24 character id?')
   })
 })
 
