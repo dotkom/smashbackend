c6dec1cb4268612d87fe5738ef7e11cc71cb5c45
haattis <exburn0@gmail.com>
2019-03-04 16:56:18 +0100

    First OIDC setup

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,50 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+//const LocalStrategy = require('passport-local');
 const User = mongoose.model('User');
+const { Stategy, Issuer } = require('openid-client')
+
+const params = {
+  client_id: process.env.CLIENT_ID,
+  redirect_uri: process.env.REDIRECT_URI,
+  scope: process.env.SCOPE,
+  client_secret: process.env.CLIENT_SECRET,
+
+}
+const passReqToCallback = false;
+const usePKCE = false;
+
+async function getOIDCClient() {
+  const provider = process.env.HOST_URL
+  const clientId = process.env.CLIENT_ID
+
+  const { Client } = await Issuer.discover(provider)
+
+  return new Client({client_id: clientId})
+}
+
+async function configureOIDCPassport(){
+  passport.use('oidc', new Strategy({
+    client,
+    params,
+    passReqToCallback,
+    usePKCE
+  }, async (tokenset, userinfo, done) => {
+    done(null, await createUser(parseOpenIDUserinfo(userinfo)))
+  }))
+}
+
+async function setupOIDC(){
+  try {
+    const client = await getOIDCClient();
+    await
+  }
+}
+
+passport.use('oidc', new Strategy({
+
+}))
+
 
 
 passport.use(new LocalStrategy({
