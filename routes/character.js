0b9fc16fff8703e5edd6f02f37c271a5b40f526a
haattis <exburn0@gmail.com>
2019-04-02 12:08:09 +0200

    Route to return stats about all characters

--- routes/character.js
+++ routes/character.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Character = mongoose.model('Character');
+const Match = mongoose.model('Match');
 
 router.get('/all', (req, res) => {
   Character.find({})
@@ -11,6 +12,22 @@ router.get('/all', (req, res) => {
     .select('name id _id')
     .then(characters => res.json(characters))
     .catch(() => res.status(400).send('Could not fetch characters'));
+});
+
+router.get('/stats', async (req, res) => {
+  const matchlist = await Match.find()
+    .populate('character1', 'name id _id')
+    .populate('character2', 'name id _id');
+
+  const array = {};
+
+  matchlist.forEach((match) => {
+    array[match.character1.id] = (array[match.character1.id] || 0) + 1;
+    array[match.character2.id] = (array[match.character2.id] || 0) + 1;
+  });
+
+
+  return res.status(200).send(array);
 });
 
 module.exports = router;
