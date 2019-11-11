bb0829962d22788055a901d0be2a9971663505d1
haattis <exburn0@gmail.com>
2019-11-11 14:49:21 +0100

    reset plz

--- config/setup.js
+++ config/setup.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose');
-const characters = require('./characters.json');
+const mongoose = require("mongoose");
+const characters = require("./characters.json");
 
-const Character = mongoose.model('Character');
-const User = mongoose.model('User');
+const Character = mongoose.model("Character");
+const User = mongoose.model("User");
 
 exports.fillDatabase = () => {
-  characters.forEach((element) => {
+  characters.forEach(element => {
     Character.updateOne(
       { id: element.id },
       { id: element.id, name: element.name },
       { upsert: true },
-      () => {}, // dirty fix
+      () => {} // dirty fix
     );
   });
 };
 
 exports.setupAdmin = () => {
-  User.updateOne({ onlineId: '600' }, { isAdmin: true, isBanned: false }, () => {});
+  User.updateOne(
+    { onlineId: "600" },
+    { isAdmin: true, isBanned: false },
+    () => {}
+  );
 };
